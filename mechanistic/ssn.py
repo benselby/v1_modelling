e0b026a7fbd0dbfdf9259b2dcd062d16e1b6815c
Ben Selby <bselby@uwaterloo.ca>
2015-11-12 00:43:15 -0700

    Added/fixeds SSNetwork class to allow OO-instantiation of SSN, and also adding a notebook for using hyperopt on SSN

--- mechanistic/ssn.py
+++ mechanistic/ssn.py
@@ -10,144 +10,95 @@ import numpy as np
 import scipy.io
 import matplotlib.image as mpimg
 
-#class SSNetwork:
+class SSNetwork:
 
-#	# Default constructor - use the Rubin et al. parameters to produce a SSN layer:
-#	# Uses the 
-#	def __init__(self):
-#		self.N_pairs = 75 # no. of E/I pairs to a side of a grid
-#		self.field_size = 16. # size of field to a side (degrees)
-#		self.dx = field_size / N_pairs
+    # Default constructor - use the Rubin et al. parameters to produce a SSN layer:
+    def __init__(self, N_pairs=75, sig_EE=8, sig_IE=12, sig_EI=4, sig_II=4, J_EE=0.1, J_IE=0.38, J_EI=0.089, J_II=0.096, OP_map=0, OD_map=0):
 
-#		self.sig_FF = 32.
-#		self.sig_RF = dx
+        self.N_pairs = N_pairs # no. of E/I pairs to a side of a grid
+        self.field_size = 16. # size of field to a side (degrees)
+        self.dx = field_size / N_pairs
 
-#		self.k   = np.random.normal(0.012, 0.05*0.012, (N_pairs, N_pairs))
-#		self.n_E = np.random.normal(2.0, 0.05*2.0, (N_pairs, N_pairs)) 
-#		self.n_I = np.random.normal(2.2, 0.05*2.2, (N_pairs, N_pairs))
+        self.sig_FF = 32.
+        self.sig_RF = dx
 
-#		self.tau_E = np.random.normal(0.02, 0.05*0.02, (N_pairs, N_pairs))
-#		self.tau_I = np.random.normal(0.01, 0.05*0.01, (N_pairs, N_pairs))
+        self.k   = np.random.normal(0.012, 0.05*0.012, (N_pairs, N_pairs))
+        self.n_E = np.random.normal(2.0, 0.05*2.0, (N_pairs, N_pairs)) 
+        self.n_I = np.random.normal(2.2, 0.05*2.2, (N_pairs, N_pairs))
 
-#		# Connection weight parameters (from supp. materials S1.1.2):
-#		self.kappa_E = 0.1
-#		self.kappa_I = 0.5
+        self.tau_E = np.random.normal(0.02, 0.05*0.02, (N_pairs, N_pairs))
+        self.tau_I = np.random.normal(0.01, 0.05*0.01, (N_pairs, N_pairs))
 
-#		# kappa_E = 0.18
-#		# kappa_I = .85
+        # Connection weight parameters (from supp. materials S1.1.2):
+        self.kappa_E = 0.1
+        self.kappa_I = 0.5
 
-#		self.J_EE = 0.1
-#		self.J_IE = 0.38
-#		self.J_EI = 0.089
-#		self.J_II = 0.096
+        # kappa_E = 0.18
+        # kappa_I = .85
 
-#		self.sig_EE = 8*dx
-#		self.sig_IE = 12*dx
-#		self.sig_EI = 4*dx
-#		self.sig_II = 4*dx
-#		self.sig_ori = 45.
-#		
-#		# load OP map from Bryan's extracted Kaschube map
-#		data = scipy.io.loadmat('orientation-map.mat')
-#		self.OP_map = data['map']
+        self.J_EE = J_EE
+        self.J_IE = J_IE
+        self.J_EI = J_EI
+        self.J_II = J_II
 
-##		self.OD_map = load_OD_map()
+        self.sig_EE = sig_EE*dx
+        self.sig_IE = sig_IE*dx
+        self.sig_EI = sig_EI*dx
+        self.sig_II = sig_II*dx
+        self.sig_ori = 45.
 
-#		[self.W_EE, self.W_IE, self.W_EI, self.W_II] = generate_connetion_weights( self.N_pairs, self.field_size, self.OP_map, self.kappa_E, self.kappa_I, self.J_EE, self.J_IE, self.J_EI, self.J_II, self.sig_EE, self.sig_IE, self.sig_EI, self.sig_II, self.sig_ori )
+        if OP_map == 0:
+            try:
+                # load OP map from Bryan's extracted Kaschube map
+                data = scipy.io.loadmat('orientation-map.mat')
+                self.OP_map = data['map']
+            except ValueError:
+                raise ValueError("Could not find orientation-map.mat!")
 
-#		self.sum_field_sizes_E = np.zeros((self.N_pairs, self.N_pairs)) 
-#		self.sum_field_sizes_I = np.zeros((self.N_pairs, self.N_pairs)) 
+        if OD_map == 0:
+            self.OD_map = load_OD_map()
 
-#	def run_simulation(self, c, h, init_cond=np.zeros( (2, self.N_pairs, self.N_pairs) ) ):
-#	    r_E = np.zeros((timesteps, self.N_pairs, self.N_pairs))
-#	    r_I = np.copy(r_E)
+        [self.W_EE, self.W_IE, self.W_EI, self.W_II] = generate_connetion_weights( self.N_pairs, self.field_size, self.OP_map, self.kappa_E, self.kappa_I, self.J_EE, self.J_IE, self.J_EI, self.J_II, self.sig_EE, self.sig_IE, self.sig_EI, self.sig_II, self.sig_ori, quiet=True )
 
-#	    # add initial conditions:
-#	    r_E[0,:,:] = init_cond[0]
-#	    r_I[0,:,:] = init_cond[1]
+        self.sum_field_sizes_E = np.zeros((self.N_pairs, self.N_pairs)) 
+        self.sum_field_sizes_I = np.zeros((self.N_pairs, self.N_pairs)) 
 
-#	    I_E = np.zeros((timesteps, self.N_pairs, self.N_pairs))
-#	    I_I = np.copy(I_E)
-#	    # rSS_E = np.copy(I_E)
-#	    # rSS_I = np.copy(I_I)
+    def run_simulation(self, dt, timesteps, c, h, init_cond=np.zeros( (2, 75, 75) ) ):
+        r_E = np.zeros((timesteps, self.N_pairs, self.N_pairs))
+        r_I = np.copy(r_E)
 
-#	    for t in range(1,timesteps):
-#	        # Input drive from external input and network
-#	        I_E[t,:,:] = c*h + np.sum( np.sum( self.W_EE * r_E[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T - np.sum( np.sum( self.W_EI * r_I[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T 
-#	        I_I[t,:,:] = c*h + np.sum( np.sum( self.W_IE * r_E[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T - np.sum( np.sum( self.W_II * r_I[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T 
-#	        
-#	        # steady state firing rates - power law I/O
-#	        rSS_E = np.multiply(self.k, np.power(np.fmax(0,I_E[t,:,:]), self.n_E))
-#	        rSS_I = np.multiply(self.k, np.power(np.fmax(0,I_I[t,:,:]), self.n_I))
+        # add initial conditions:
+        r_E[0,:,:] = init_cond[0]
+        r_I[0,:,:] = init_cond[1]
 
-#	        # set negative steady state rates to zero
-#	        rSS_E[rSS_E < 0] = 0
-#	        rSS_I[rSS_I < 0] = 0
+        I_E = np.zeros((timesteps, self.N_pairs, self.N_pairs))
+        I_I = np.copy(I_E)
+        # rSS_E = np.copy(I_E)
+        # rSS_I = np.copy(I_I)
 
-#	        # instantaneous firing rates approaching steady state
-#	        r_E[t,:,:] = r_E[t-1,:,:] + dt*(np.divide(-r_E[t-1,:,:]+rSS_E, self.tau_E))
-#	        r_I[t,:,:] = r_I[t-1,:,:] + dt*(np.divide(-r_I[t-1,:,:]+rSS_I, self.tau_I))
-#	        
-#	    return [r_E, r_I, I_E, I_I]
+        for t in range(1,timesteps):
+            # Input drive from external input and network
+            I_E[t,:,:] = c*h + np.sum( np.sum( self.W_EE * r_E[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T - np.sum( np.sum( self.W_EI * r_I[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T 
+            I_I[t,:,:] = c*h + np.sum( np.sum( self.W_IE * r_E[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T - np.sum( np.sum( self.W_II * r_I[t-1,:,:],1 ), 1 ).reshape(self.N_pairs, self.N_pairs).T 
+            
+            # steady state firing rates - power law I/O
+            rSS_E = np.multiply(self.k, np.power(np.fmax(0,I_E[t,:,:]), self.n_E))
+            rSS_I = np.multiply(self.k, np.power(np.fmax(0,I_I[t,:,:]), self.n_I))
 
-  #   # function to reproduce figure 6B of Rubin et al, 2015, showing the SSN 
-  #   # transition from external to network drive with dominant inhibition
-  #   def plot_network_contrast_response(self, r_units=np.floor( self.N_pairs*np.random.rand(25,2) ), c_range=np.linspace(3, 50, 12) ):
-		# pass
+            # set negative steady state rates to zero
+            rSS_E[rSS_E < 0] = 0
+            rSS_I[rSS_I < 0] = 0
 
-def load_OP_map(filename):
-	pass
+            # instantaneous firing rates approaching steady state
+            r_E[t,:,:] = r_E[t-1,:,:] + dt*(np.divide(-r_E[t-1,:,:]+rSS_E, self.tau_E))
+            r_I[t,:,:] = r_I[t-1,:,:] + dt*(np.divide(-r_I[t-1,:,:]+rSS_I, self.tau_I))
+            
+        return [r_E, r_I, I_E, I_I]
 
-#def load_OD_map(filename='ocular-dom-obermayer-fig1.png'):
-#	# Ocular dominance map: from Obermayer and Blasdel, 1993
-#	# which contains images of ocular dominance and orientation preference from the same region
-
-#	def rgb2gray(rgb):
-#	    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-
-#	OD_raw = mpimg.imread(filename)    
-#	OD_gray = rgb2gray(OD_raw)    
-
-#	# subsample the Blasdel images to get (approximately) the right scale of OD map
-#	inds = np.round(np.linspace(0,OD_gray.shape[0], N_pairs, False)).astype(int)
-#	OD_img = OD_gray[inds[:,np.newaxis],inds+OD_gray.shape[1]-OD_gray.shape[0]]
-
-#	OD_norm = (OD_img - np.min(OD_img) ) / np.max(OD_img - np.min(OD_img))
-
-#	OD_img_hist, OD_img_bins = np.histogram(OD_norm, bins=256)
-
-#	OD_img_CDF = np.cumsum(np.array(OD_img_hist).astype(float)/np.sum(OD_img_hist))
-
-#	# Scale ocular dominance map based on complex cell distribution in 
-#	# layer 3, Hubel and Wiesel 1968, fig. 14, wherein cells are divided into
-#	# 7 groups, 1 meaning total preference for the ipsilateral eye, and 7 for
-#	# total contralateral dominance
-#	OD_target_dist = np.array([26., 22., 17., 9., 30., 37., 35.])
-#	OD_target_prop = OD_target_dist/np.sum(OD_target_dist)
-#	OD_target_CDF = np.cumsum(OD_target_prop)
-
-#	ocularity_vals = np.linspace(0,1, len(OD_target_dist), endpoint=True)
-
-#	boundaries = np.zeros(len(OD_target_dist)-1)
-#	j = 0
-#	for i in range(len(OD_img_CDF)):
-#	    if OD_img_CDF[i] >= OD_target_CDF[j] and j < len(OD_target_dist)-1:
-#	        boundaries[j] = i/256.
-#	        j = j+1
-#	        
-#	OD_map = np.zeros_like(OD_norm)
-
-#	for i in range(len(boundaries)):
-#	    if i == 0:
-#	        OD_map[OD_norm <= boundaries[i]] = ocularity_vals[0]
-#	    elif i == len(boundaries)-1:
-#	        OD_map[OD_norm > boundaries[i]] = ocularity_vals[-1]
-#	        OD_map[np.logical_and(OD_norm < boundaries[i], OD_norm >= boundaries[i-1])] = ocularity_vals[-2]
-#	    else:
-#	        OD_map[np.logical_and(OD_norm <= boundaries[i], OD_norm > boundaries[i-1])] = ocularity_vals[i]
-
-#    return OD_map
-
+    # function to reproduce figure 6B of Rubin et al, 2015, showing the SSN 
+    # transition from external to network drive with dominant inhibition
+    def plot_network_contrast_response(self, r_units=np.floor( 75*np.random.rand(25,2) ), c_range=np.linspace(3, 50, 12) ):
+        pass
     
 def diff(x,y):
     return np.abs( np.mod( x - y + 90, 180) - 90 )
@@ -208,7 +159,7 @@ def generate_ext_stimulus(ori, size, centre, OP_map, OD_map, ocularity, sig_RF=1
     
     return h
 
-def generate_mono_stimulus(ori, size, centre, OP_map, sig_RF=16./75, sig_FF = 32., fsize=16., full_frame=False):
+def generate_mono_stimulus(ori, size, centre, OP_map, sig_RF=16./75, sig_FF=32., fsize=16., full_frame=False):
     if centre[0] > fsize or centre[1] > fsize:
         raise ValueError('Centre of stimulus is off the grid of neurons!')
         
@@ -268,7 +219,7 @@ def generate_ring_stimulus(orientation, inner_d, outer_d, centre, ocularity, OP_
     return h
 
 # randomly generate connection weights for all the units in a square grid
-def generate_connetion_weights( N_pairs, field_size, OP_map, kappa_E, kappa_I, J_EE, J_IE, J_EI, J_II, sig_EE, sig_IE, sig_EI, sig_II, sig_ori ):
+def generate_connetion_weights( N_pairs, field_size, OP_map, kappa_E, kappa_I, J_EE, J_IE, J_EI, J_II, sig_EE, sig_IE, sig_EI, sig_II, sig_ori , quiet=False):
 	xy_range = np.linspace(0, field_size, N_pairs, False)
 
 	xv, yv = np.meshgrid(xy_range, xy_range) # x and y grid values (degrees)
@@ -340,8 +291,8 @@ def generate_connetion_weights( N_pairs, field_size, OP_map, kappa_E, kappa_I, J
 	u_IE = mean_connections(W_IE)
 	u_EI = mean_connections(W_EI)
 	u_II = mean_connections(W_II)
-
-	print "Mean no. of connections:\nu_EE: %d\t u_IE: %d\t u_EI: %d\t u_II: %d" % (u_EE, u_IE, u_EI, u_II)
+	if quiet==False:
+		print "Mean no. of connections:\nu_EE: %d\t u_IE: %d\t u_EI: %d\t u_II: %d" % (u_EE, u_IE, u_EI, u_II)
 
 	# For non-zero connections, determine the weight 
 	W_EE[W_EE != 0] = np.random.normal(J_EE, 0.25*J_EE, W_EE[W_EE!=0].size)
@@ -375,17 +326,17 @@ def generate_connetion_weights( N_pairs, field_size, OP_map, kappa_E, kappa_I, J
     # From S.1.3.2: for strongest nonlinear behaviour, omega_E < 0 and omega_E < omega_I
 	# where omega_E = sum(W_II) - sum(W_EI), omega_I = sum(W_IE) - sum(W_EE)
 	# Verify here:
+	if quiet==False:
+		omega_E = np.sum(W_II) - np.sum(W_EI)
+		omega_I = np.sum(W_IE) - np.sum(W_EE)
 
-	omega_E = np.sum(W_II) - np.sum(W_EI)
-	omega_I = np.sum(W_IE) - np.sum(W_EE)
+		print 'Omega_E: ', omega_E
+		print 'Omega_I: ', omega_I
 
-	print 'Omega_E: ', omega_E
-	print 'Omega_I: ', omega_I
-
-	if omega_E < 0 and omega_I > omega_E:
-	    print "System should show strong nonlinear behaviour!"
-	else:
-	    print "System may not show strong nonlinear behaviour."
+		if omega_E < 0 and omega_I > omega_E:
+		    print "System should show strong nonlinear behaviour!"
+		else:
+		    print "System may not show strong nonlinear behaviour."
 
 	return [W_EE, W_IE, W_EI, W_II]
 
